431f62f2b116e2e950422db5b880a580468db6ed
Viktorionn <viktordmiletic@gmail.com>
2026-03-25 12:16:36 -0500

    Repo update

--- Project_API_Pokemon/run_module.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [string]$ModuleName
-)
-
-# Ruta al intérprete de Python de tu entorno virtual
-$pythonPath = "C:/Proyectos/Python/.ConquerBlocks/Scripts/python.exe"
-
-# Ejecuta el módulo con -m
-& $pythonPath -m $ModuleName
--- Project_API_Pokemon/test_preload.py
+++
@@ -1,4 +0,0 @@
-from services.pokemon_service import preload_pokemon_range
-
-# Probar precarga de los primeros 10 Pokémon
-preload_pokemon_range(1, 1025, max_workers=5)
